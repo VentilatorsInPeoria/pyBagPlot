9646d3f62f222e3970d78249e95362b33eb115ee
James Lewellyn <lewellyn.james@gmail.com>
2020-08-29 20:28:56 -0500

    add logging and poetry script functinality

--- pybagplot/BagPlot.py
+++ pybagplot/BagPlot.py
@@ -3,6 +3,9 @@
 
 from __future__ import annotations
 
+import argparse
+import logging
+
 import pandas as pd
 
 
@@ -36,3 +39,27 @@ class PlotBagTest:
         """Execute run functionality."""
         df = self.loadfile()
         df.plot(figsize=(10, 12), subplots=True, grid=True, title=self.title)
+
+
+def main() -> int:
+    """Do the main stuff."""
+    # Get arguments
+    parser = argparse.ArgumentParser(description="Process some integers.")
+    parser.add_argument(
+        "-f",
+        "--filen",
+        required=True,
+        help="filename to plot",
+    )
+    parser.add_argument(
+        "-t",
+        "--title",
+        required=True,
+        help="title of plot",
+    )
+    args = parser.parse_args()
+    logging.info("Session started.")
+    logging.info("Name: %s", args.filen)
+    logging.info("Title: %s", args.title)
+    pbt = PlotBagTest(args.filen, args.title)
+    pbt.run()
--- pybagplot/__init__.py
+++ pybagplot/__init__.py
@@ -1,2 +1,7 @@
 #!/usr/bin/env python3
 """Initialize the package."""
+
+import logging
+
+# Use root python logger to set loglevel to INFO
+logging.basicConfig(level=logging.INFO)
